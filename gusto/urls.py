f36116601c6cb0c8039792e5051c12c23ed671ca
BuildTools <unconfigured@null.spigotmc.org>
2021-01-19 15:40:17 +0200

    Edited

--- gusto/urls.py
+++ gusto/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from gusto import hello
+from gusto.views import hello
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin', admin.site.urls),
     path('', hello)
 ]
