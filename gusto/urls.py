951a10d11f035dc3ec61c0edaebd5856fb7eec4b
BuildTools <unconfigured@null.spigotmc.org>
2021-01-26 15:35:35 +0200

    26.01

--- gusto/urls.py
+++ gusto/urls.py
@@ -15,10 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf.urls.static import static
 from gusto.views import hello
+from gusto import settings
 
 urlpatterns = [
     path('admin', admin.site.urls),
     path('', include('main_gusto.urls')),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- main_gusto/views.py
+++ main_gusto/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Category, Dish
+
 
 def main(request):
-    return render(request, 'index.html')
+    categories = Category.objects.filter(is_visible=True).order_by('category_order')
+    for item in categories:
+        item.dishes = Dish.objects.filter(category=item.pk)
+    return render(request, 'index.html', context={'categories': categories})
 # Create your views here.
