afdec9abbf3a732b13041d72520e3ce2391c6888
BuildTools <unconfigured@null.spigotmc.org>
2021-02-02 15:00:27 +0200

    02.02

--- main_gusto/views.py
+++ main_gusto/views.py
@@ -1,9 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Category, Dish, Event, Banners
 import datetime
+from .forms import UserMessageForm
 
 
 def main(request):
+    if request.method == 'POST':
+        form = UserMessageForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+
     special_categories = Category.objects.filter(is_visible=True).filter(is_special=True).order_by('category_order')
     for item in special_categories:
         item.dishes = Dish.objects.filter(category=item.pk)
@@ -16,9 +23,12 @@ def main(request):
 
     banners = Banners.objects.filter(is_visible=True)
 
+    users_messages_form = UserMessageForm()
+
     return render(request, 'index.html', context={'categories': categories,
                                                   'special_categories': special_categories,
                                                   'events': events,
                                                   'banners': banners,
+                                                  'form': users_messages_form,
                                                   }
                   )
--- menu_gusto/views.py
+++ menu_gusto/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from main_gusto.models import Dish
 # Create your views here.
+
+
 def dish_info(request, pk):
-    dish= Dish.objects.get(pk=pk)
-    return render(request, 'dish.html', context= {
-        'dish':dish,
-    })
+    dish = Dish.objects.get(pk=pk)
+    return render(request, 'dish.html', context={
+        'dish': dish,
+    }
+                  )
